58343c2c65dda4ebd04c17ec3f5a358c5a2f519e
wolf011 <duckhouse_bousoukis@aleeas.com>
2025-05-08 11:27:51 -0300

    atualizações das aulas

---
+++ monitoria/js/script2.js
@@ -0,0 +1,11 @@
+console.log("Teste!");
+
+// const promise = new Promise( function(resolve, reject) {
+    
+// });
+
+setTimeout( function() {
+    console.log("Primeiro console.log");
+}, 2000);
+
+console.log("Segundo console.log");
